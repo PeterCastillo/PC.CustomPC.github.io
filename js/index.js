377f5aa48174578806a3e25a3824b1a524ac58f4
PeterCastillo <peterjackcc@gmail.com>
2022-08-17 14:35:23 -0500

    JS

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ fetch(url)
 
 const seleccion = document.querySelector('.custom__selected');
 const container = document.querySelector('.custom__list__type__list');
-const tipoComponentes = document.querySelector('.custom__list__type')
+const tipoComponentes = document.querySelector('.custom__list__type');
 const fragmento = document.createDocumentFragment();
 
 seleccion.addEventListener('click', (e) => {
@@ -29,6 +29,10 @@ seleccion.addEventListener('click', (e) => {
 
 const select = e => {
     if(e.target.classList.contains('custom__selected__selected-img') || e.target.classList.contains('custom__selected__selected--hover')){
+        e.target.parentElement.parentElement.querySelectorAll('.custom__selected__selected--type').forEach(element => {
+            element.style.textAlign = "start"
+        })
+        e.target.parentElement.querySelector('.custom__selected__selected--type').style.textAlign = "end";
         sett(e.target.parentElement)
     }
 };
@@ -52,7 +56,8 @@ const pintar = (piezaTipo) => {
                 <div class="custom__selected__selected-img-hover"><div><img src="${componente.img}" alt=""></div></div>
             `
             if(componente.seleccionado){
-                div.classList.add('custom__list__type__list--element--selected')
+                div.classList.add('custom__list__type__list--element--selected');
+                pintarSeleccionado(componente);
             }
             div.innerHTML = componenteContent;
             fragmento.appendChild(div);
@@ -76,4 +81,26 @@ const addClick = (e) => {
         componentes[e.target.dataset.id].seleccionado = true;
         pintar(tipoComponenteSelecionado);
     };
+};
+
+const pintarSeleccionado = (componente) => {
+    const tipo = componentes[componente.id].type;
+    seleccion.querySelectorAll('.custom__selected__selected').forEach(elemento => {
+        if(elemento.querySelector('.custom__selected__selected--type').textContent == tipo){
+            const selecionadoContent = `
+            <div class="custom__selected__selected-img"><img src="${componente.img}" alt=""></div>
+            <span class="custom__selected__selected--type">${componente.type}</span>
+            <span class="custom__selected__selected--hover">${componente.nombre}</span>
+            `
+            elemento.innerHTML = selecionadoContent;
+        }
+    });
+    // const selecionadin = document.querySelector(`.${tipo}`);
+    // const selecionadoContent = `
+    // <div class="custom__selected__selected-img"><img src="${componente.img}" alt=""></div>
+    // <span class="custom__selected__selected--type">${componente.type}</span>
+    // <span class="custom__selected__selected--hover">${componente.nombre}</span>
+    // `
+    // selecionadin.innerHTML = selecionadoContent
+
 };
